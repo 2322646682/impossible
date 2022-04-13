3ef4335fe5261cca86e12421f4ecff8227494736
2322646682 <2322646682@qq.com>
2022-04-13 20:33:22 +0800

    Second commit, so what happened!

---
+++ src/test/java/com/example/impossible/ImpossibleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.impossible;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImpossibleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
